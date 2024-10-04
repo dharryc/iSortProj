4e24f1ca5a8fb4efd2394e339cdb4fa7bafbcee9
dharryc <dharrisoncook@gmail.com>
2024-10-04 08:43:59 -0600

    done

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,36 +1,111 @@
 namespace iSortProj;
-
+//Harrison Cook
+//Solo Project 1 - Iterative Sorting
 public static class Algorithms
 {
-    // I messed around with this quite a bit, and I think it's tail call optimized, I'm writing this as I run a test with a 500,000 int long array.
-    // IT WORKS!!! The 500,000 long array takes like 10 min, but it works for the assignment. I'm going to submit some code that's just showy for the assignment, maybe 50,000 long.
-    // I fixed it further, it's like twice as fast now!!!
-    public static int[] insertionSort(int[] a)
+    public static int[] makeRandomArray(int a) //handy method to make an array as long or short as you'd like
     {
-        //First, get a starting point for the algorithm to start making comparisons
+        int[] j = new int[a];
+
+        Random rnd = new Random();
+
+        for (int l = 0; l < a; l++)
+        {
+            j[l] = rnd.Next(0, 101);
+        }
+
+        return j;
+    }
+
+    public static int[] InsertionSort(int[] a)
+    {
         int index = 1;
-        //Create a placeholder so you can get back to the right place once you've "inserted" your element into the right place
         int saveIndex = index;
-        //Now make sure that the algorithm inspects each element in the array
         while (index < a.Length)
         {
-            //Check if the current element is smaller than the element behind it
             if (a[index] < a[index - 1])
             {
-                //Swap values
                 (a[index], a[index - 1]) = (a[index - 1], a[index]);
-                //After you make the swap you have to check back through until you hit values that are the same as your current value
                 while (index > 1 && a[index - 1] < a[index - 2])
                 {
                     index--;
                     (a[index], a[index - 1]) = (a[index - 1], a[index]);
                 }
             }
-            //Go back through your loop at the next index
             index = saveIndex++;
         }
-        //Return the sorted index
         return a;
+    }
+
+
+    public static int[] SelectionSort(int[] a)
+    {
+        for (int index = 0; index < a.Length; index++) //run through the array
+        {
+            int minIndex = FindMin(a, index);
+
+            (a[index], a[minIndex]) = (a[minIndex], a[index]);
+        }
+
+        return a;
+    }
+
+    private static int FindMin(int[] a, int index)
+    {
+        int min = a[index]; // assume the start is the smallest
+
+        int minIndex = index; //store the starting value
+
+        for (int b = index + 1; b < a.Length; b++) //check the next value and every subsequent value
+        {
+            if (a[b] < min) //if any of those subsequent values are smaller, store that value and location, then continue comparing from there.
+            {
+                min = a[b];
+
+                minIndex = b;
+            }
+        }
+
+        return minIndex; //return the sorted list
+    }
+
+    public static int[] SlowerSelectionSort(int[] a)
+    {
+        for (int index = 0; index < a.Length; index++) //run through the array
+        {
+            int minIndex = FindMinShortCircuit(a, index);
+
+            (a[index], a[minIndex]) = (a[minIndex], a[index]);
+        }
+
+        return a;
+    }
+
+    private static int FindMinShortCircuit(int[] a, int index)
+    {
+        if (index == 0)
+            return FindMin(a, index);
+
+        int runningMin = a[index - 1]; //store the previous value as a "running min" to make sure you can skip repeating numbers
+
+        int min = a[index]; // assume the start is the smallest
+
+        int minIndex = index; //store the starting value
+
+        for (int b = index; b < a.Length; b++) //check the next value and every subsequent value
+        {
+            if (a[b] <= min) //if any of those subsequent values are smaller, store that value and location, then continue comparing from there.
+            {
+                if (a[b] == runningMin)
+                    return b; //but if you run into a number and you know you won't find a smaller one, break out and stop looking
+
+                min = a[b];
+
+                minIndex = b;
+            }
+        }
+
+        return minIndex; //return the sorted list
     }
 }
 
@@ -39,8 +114,76 @@ public class Homework79Problem3B
     [Fact]
     public void Tests()
     {
-        int[] a = { 7, 5, 2, 3, 9, 8, 0, 9, 3, 4, 6, 8, 3, 1, 6, 8, 9, 1, 0, 9, 4, 1, 5, 7, 99, 76, 54, 2, 2, 345, 5 };
-        int[] b = { 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 7, 7, 8, 8, 8, 9, 9, 9, 9, 54, 76, 99, 345 };
-        Assert.Equal(b, Algorithms.insertionSort(a));
+        int[] x = Algorithms.makeRandomArray(20000);
+        int[] a =
+        {
+            7,
+            5,
+            2,
+            3,
+            9,
+            8,
+            0,
+            9,
+            3,
+            4,
+            6,
+            8,
+            3,
+            1,
+            6,
+            8,
+            9,
+            1,
+            0,
+            9,
+            4,
+            1,
+            5,
+            7,
+            99,
+            76,
+            54,
+            2,
+            2,
+            345,
+            5,
+        };
+        int[] b =
+        {
+            0,
+            0,
+            1,
+            1,
+            1,
+            2,
+            2,
+            2,
+            3,
+            3,
+            3,
+            4,
+            4,
+            5,
+            5,
+            5,
+            6,
+            6,
+            7,
+            7,
+            8,
+            8,
+            8,
+            9,
+            9,
+            9,
+            9,
+            54,
+            76,
+            99,
+            345,
+        };
+        Assert.Equal(b, Algorithms.InsertionSort(a));
+        Assert.Equal(Algorithms.InsertionSort(x), Algorithms.SelectionSort(x));
     }
 }
--- obj/Debug/net8.0/iSortProj.AssemblyInfo.cs
+++ obj/Debug/net8.0/iSortProj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iSortProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+751533fca6b5521137e3b6fcd6d8504fff097084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2483a0cd160be85cfe02e451408eaf14f28eea3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("iSortProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iSortProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
